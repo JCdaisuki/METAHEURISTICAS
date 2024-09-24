bb97396e9786422013fff8436adf8ae4d485c53e
JCdaisuki <99610897+JCdaisuki@users.noreply.github.com>
2024-09-24 17:00:01 +0200

    Arreglo al escoger ciudades
    Creo que tienes razón tú en esto.
    Se debe escoger una ciudad aleatoria de entre todas, sino siempre saldía el mismo camino al hacer:
    for( i < k){random.nextInt(k)}
    Porque se acabarían escogiendo todos los valores.

--- PRACTICA_1/src/Algoritmos/Greedy.java
+++ PRACTICA_1/src/Algoritmos/Greedy.java
@@ -67,7 +67,7 @@ public class Greedy
 
         for(int i = 0; i < k; i++)
         {
-            ciudad = random.nextInt(k);
+            ciudad = random.nextInt(ordenado.size());
 
             if (!visitada[ciudad])
             {
